74c28ff79b2013b6744a448f7ca7e7bea64d2e39
wehba <Lenovo@wehba.com>
2021-11-15 10:13:00 +0100

    send to github

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+function Home( ) {
+  
+  return (
+    <div style={{textAlign:'center', margin:'20px',backgroundColor:'lightgreen'}}>
+      <h2>Welcome to third section computer scientist</h2>
+      <h3>Jons videos explaining</h3>
+      <p>In this single web page application we have: </p>
+        <ul style={{ listStyleType: "none"}}>
+          <li>1-Securit(JWT)</li>
+          <li>2-Thread</li>
+          <li>3-Routering</li>
+        </ul>
+    </div>
+  );
+}
+
+export default Home;
